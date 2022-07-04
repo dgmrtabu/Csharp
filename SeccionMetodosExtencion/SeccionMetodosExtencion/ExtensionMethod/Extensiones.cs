67d8ba87125a2c6d638a0b9d98d85d5c36cb69cc
dgmrtabu <dgmr.tabu@gmail.com>
2022-07-04 19:56:41 -0400

    104 - Datatime obtener inicio y termino de semana

---
+++ SeccionMetodosExtencion/SeccionMetodosExtencion/ExtensionMethod/Extensiones.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeccionMetodosExtencion.ExtensionMethod
+{
+    public static class Extensiones
+    {
+        public static int Ext_CalcularEdad(this DateTime valor)
+        {
+            return DateTime.Now.AddTicks(-valor.Ticks).Year - 1;
+        }
+
+        public static string Ext_MesNombre(this DateTime valor)
+        {
+            DateTimeFormatInfo info = new CultureInfo("en-us", false).DateTimeFormat;
+            return info.GetMonthName(valor.Month);
+        }
+
+        public static DateTime Ext_ObtenerMesInicio(this DateTime valor)
+        {
+            return new DateTime(valor.Year, valor.Month, 1);
+        }
+        public static DateTime Ext_ObtenerMesTermino(this DateTime valor)
+        {
+            return new DateTime(valor.Year, valor.Month, DateTime.DaysInMonth(valor.Year, valor.Month));
+        }
+
+        public static DateTime Ext_ObtenerSemanaInicio(this DateTime valor)
+        {
+            var fecha = valor;
+            var dias = 1 - (fecha.DayOfWeek != DayOfWeek.Sunday ? (double)fecha.DayOfWeek : 7d);
+            return fecha.AddDays(dias);
+            
+        }
+        public static DateTime Ext_ObtenerSemanaTermino(this DateTime valor)
+        {
+            var fecha = valor;
+            var dias = 7 - (fecha.DayOfWeek != DayOfWeek.Sunday ? (double)fecha.DayOfWeek : 7d);
+            return fecha.AddDays(dias);
+        }
+    }
+}
---
+++ SeccionMetodosExtencion/SeccionMetodosExtencion/Program.cs
@@ -0,0 +1,31 @@
+﻿using SeccionMetodosExtencion.ExtensionMethod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeccionMetodosExtencion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtencionesFecha();
+            Console.Read();
+        }
+
+        static void ExtencionesFecha()
+        {
+            var fecha1 = new DateTime(2000, 1, 1);
+            var fecha2 = new DateTime(2000, 12, 31);
+            Console.WriteLine($"La fecha es {fecha1.ToString("dd-MM-yyyy")} la edad {fecha1.Ext_CalcularEdad()}");
+            Console.WriteLine($"La fecha es {fecha2.ToString("dd-MM-yyyy")} la edad {fecha2.Ext_CalcularEdad()}");
+            Console.WriteLine($"El mes de la fecha 1 es: {fecha1.Ext_MesNombre()} --- El mes de la fecha 2 es: {fecha2.Ext_MesNombre()}");
+
+            var fecha3 = new DateTime(2020, 1, 3);
+            Console.WriteLine($"Inicio de mes: {fecha3.Ext_ObtenerMesInicio().ToString("dd-MM-yyyy")} --- Termino de mes: {fecha3.Ext_ObtenerMesTermino().ToString("dd-MM-yyyy")}");
+            Console.WriteLine($"Inicio de semana: {fecha3.Ext_ObtenerSemanaInicio().ToString("dd-MM-yyyy")} --- Termino de semana: {fecha3.Ext_ObtenerSemanaTermino().ToString("dd-MM-yyyy")}");
+        }
+    }
+}
