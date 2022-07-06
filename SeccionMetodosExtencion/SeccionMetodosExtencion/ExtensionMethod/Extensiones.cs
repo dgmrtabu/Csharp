ab3987e59c9e0af3625bd19041aa20b3ab100ecf
dgmrtabu <dgmr.tabu@gmail.com>
2022-07-06 16:23:20 -0400

    107 String Fecha valida

--- SeccionMetodosExtencion/SeccionMetodosExtencion/ExtensionMethod/Extensiones.cs
+++ SeccionMetodosExtencion/SeccionMetodosExtencion/ExtensionMethod/Extensiones.cs
@@ -4,43 +4,88 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SeccionMetodosExtencion.ExtensionMethod
 {
     public static class Extensiones
     {
-        public static int Ext_CalcularEdad(this DateTime valor)
-        {
-            return DateTime.Now.AddTicks(-valor.Ticks).Year - 1;
-        }
+        //public static int Ext_CalcularEdad(this DateTime valor)
+        //{
+        //    return DateTime.Now.AddTicks(-valor.Ticks).Year - 1;
+        //}
 
-        public static string Ext_MesNombre(this DateTime valor)
-        {
-            DateTimeFormatInfo info = new CultureInfo("en-us", false).DateTimeFormat;
-            return info.GetMonthName(valor.Month);
-        }
+        //public static string Ext_MesNombre(this DateTime valor)
+        //{
+        //    DateTimeFormatInfo info = new CultureInfo("en-us", false).DateTimeFormat;
+        //    return info.GetMonthName(valor.Month);
+        //}
 
-        public static DateTime Ext_ObtenerMesInicio(this DateTime valor)
-        {
-            return new DateTime(valor.Year, valor.Month, 1);
-        }
-        public static DateTime Ext_ObtenerMesTermino(this DateTime valor)
-        {
-            return new DateTime(valor.Year, valor.Month, DateTime.DaysInMonth(valor.Year, valor.Month));
-        }
+        //public static DateTime Ext_ObtenerMesInicio(this DateTime valor)
+        //{
+        //    return new DateTime(valor.Year, valor.Month, 1);
+        //}
+        //public static DateTime Ext_ObtenerMesTermino(this DateTime valor)
+        //{
+        //    return new DateTime(valor.Year, valor.Month, DateTime.DaysInMonth(valor.Year, valor.Month));
+        //}
 
-        public static DateTime Ext_ObtenerSemanaInicio(this DateTime valor)
-        {
-            var fecha = valor;
-            var dias = 1 - (fecha.DayOfWeek != DayOfWeek.Sunday ? (double)fecha.DayOfWeek : 7d);
-            return fecha.AddDays(dias);
+        //public static DateTime Ext_ObtenerSemanaInicio(this DateTime valor)
+        //{
+        //    var fecha = valor;
+        //    var dias = 1 - (fecha.DayOfWeek != DayOfWeek.Sunday ? (double)fecha.DayOfWeek : 7d);
+        //    return fecha.AddDays(dias);
             
-        }
-        public static DateTime Ext_ObtenerSemanaTermino(this DateTime valor)
+        //}
+        //public static DateTime Ext_ObtenerSemanaTermino(this DateTime valor)
+        //{
+        //    var fecha = valor;
+        //    var dias = 7 - (fecha.DayOfWeek != DayOfWeek.Sunday ? (double)fecha.DayOfWeek : 7d);
+        //    return fecha.AddDays(dias);
+        //}
+
+        //public static (DateTime, DateTime) Ext_ObtenerMesInicioTermino(this DateTime valor)
+        //{
+        //    return (valor.Ext_ObtenerMesInicio(), valor.Ext_ObtenerMesTermino());
+        //}
+
+        //public static (DateTime, DateTime) Ext_ObtenerSemanaInicioTermino(this DateTime valor)
+        //{
+        //    return (valor.Ext_ObtenerSemanaInicio(), valor.Ext_ObtenerSemanaTermino());
+        //}
+
+        public static bool Ext_EsNumeroEnteroValido(this string valor)
         {
-            var fecha = valor;
-            var dias = 7 - (fecha.DayOfWeek != DayOfWeek.Sunday ? (double)fecha.DayOfWeek : 7d);
-            return fecha.AddDays(dias);
+            Regex re = new Regex(@"^[0-9]+$");
+            return re.IsMatch(valor);
+        }
+
+        public static bool Ext_EsPorcentajeValido(this string valor)
+        {
+            Regex re = new Regex(@"^[0-9]{1,2}([\,][0-9]{1,3})?$");
+            return re.IsMatch(valor);
+        }
+
+        public static bool Ext_EsFechaValida(this string valor)
+        {
+            var esFechaValida = false;
+
+            try
+            {
+                string[] formatos = { "dd-MM-yyyy", "yyyy-MM-dd" };
+                esFechaValida = DateTime.TryParseExact(valor, formatos, new CultureInfo("es-cl"), DateTimeStyles.None, out var fecha);
+            }
+            catch (Exception) {
+                esFechaValida = false;
+            }
+            return esFechaValida;
+        }
+
+        public static DateTime Ext_ToFecha(this string valor)
+        {
+            string[] formatos = { "dd-MM-yyyy", "yyyy-MM-dd" };
+            DateTime.TryParseExact(valor, formatos, new CultureInfo("es-cl"), DateTimeStyles.None, out var fecha);
+            return fecha;
         }
     }
 }
--- SeccionMetodosExtencion/SeccionMetodosExtencion/Program.cs
+++ SeccionMetodosExtencion/SeccionMetodosExtencion/Program.cs
@@ -17,15 +17,43 @@ namespace SeccionMetodosExtencion
 
         static void ExtencionesFecha()
         {
-            var fecha1 = new DateTime(2000, 1, 1);
-            var fecha2 = new DateTime(2000, 12, 31);
-            Console.WriteLine($"La fecha es {fecha1.ToString("dd-MM-yyyy")} la edad {fecha1.Ext_CalcularEdad()}");
-            Console.WriteLine($"La fecha es {fecha2.ToString("dd-MM-yyyy")} la edad {fecha2.Ext_CalcularEdad()}");
-            Console.WriteLine($"El mes de la fecha 1 es: {fecha1.Ext_MesNombre()} --- El mes de la fecha 2 es: {fecha2.Ext_MesNombre()}");
+            //var fecha1 = new DateTime(2000, 1, 1);
+            //var fecha2 = new DateTime(2000, 12, 31);
+            //Console.WriteLine($"La fecha es {fecha1.ToString("dd-MM-yyyy")} la edad {fecha1.Ext_CalcularEdad()}");
+            //Console.WriteLine($"La fecha es {fecha2.ToString("dd-MM-yyyy")} la edad {fecha2.Ext_CalcularEdad()}");
+            //Console.WriteLine($"El mes de la fecha 1 es: {fecha1.Ext_MesNombre()} --- El mes de la fecha 2 es: {fecha2.Ext_MesNombre()}");
 
-            var fecha3 = new DateTime(2020, 1, 3);
-            Console.WriteLine($"Inicio de mes: {fecha3.Ext_ObtenerMesInicio().ToString("dd-MM-yyyy")} --- Termino de mes: {fecha3.Ext_ObtenerMesTermino().ToString("dd-MM-yyyy")}");
-            Console.WriteLine($"Inicio de semana: {fecha3.Ext_ObtenerSemanaInicio().ToString("dd-MM-yyyy")} --- Termino de semana: {fecha3.Ext_ObtenerSemanaTermino().ToString("dd-MM-yyyy")}");
+            //var fecha3 = new DateTime(2020, 1, 3);
+            //Console.WriteLine($"Inicio de mes: {fecha3.Ext_ObtenerMesInicio().ToString("dd-MM-yyyy")} --- Termino de mes: {fecha3.Ext_ObtenerMesTermino().ToString("dd-MM-yyyy")}");
+            //Console.WriteLine($"Inicio de semana: {fecha3.Ext_ObtenerSemanaInicio().ToString("dd-MM-yyyy")} --- Termino de semana: {fecha3.Ext_ObtenerSemanaTermino().ToString("dd-MM-yyyy")}");
+
+            //var (mesInicio, mesTermino) = fecha3.Ext_ObtenerMesInicioTermino();
+            //var (semanaInicio, semanaTermino) = fecha3.Ext_ObtenerSemanaInicioTermino();
+            //Console.WriteLine($"Inicio de semana: {semanaInicio.ToString("dd-MM-yyyy")} --- Termino de semana: {semanaTermino.ToString("dd-MM-yyyy")}");
+
+            //var semana = fecha3.Ext_ObtenerSemanaInicioTermino();
+            //Console.WriteLine($"Inicio de semana: {semana.Item1.ToString("dd-MM-yyyy")} --- Termino de semana: {semana.Item2.ToString("dd-MM-yyyy")}");
+
+            var numero1 = "1000";
+            var numero2 = "1000,25";
+            var numero3 = "17,25";
+            var numero4 = "david";
+
+            Console.WriteLine($"{numero1}: es entero: {numero1.Ext_EsNumeroEnteroValido()}, es porcentaje {numero1.Ext_EsPorcentajeValido()}");
+            Console.WriteLine($"{numero2}: es entero: {numero2.Ext_EsNumeroEnteroValido()}, es porcentaje {numero2.Ext_EsPorcentajeValido()}");
+            Console.WriteLine($"{numero3}: es entero: {numero3.Ext_EsNumeroEnteroValido()}, es porcentaje {numero3.Ext_EsPorcentajeValido()}");
+            Console.WriteLine($"{numero4}: es entero: {numero4.Ext_EsNumeroEnteroValido()}, es porcentaje {numero4.Ext_EsPorcentajeValido()}");
+
+            var fecha1 = "32-01-2020";
+            var fecha2 = "texto";
+            var fecha3 = "31-01-2020";
+            var fecha4 = "2020-01-03";
+
+            Console.WriteLine($"{fecha1}: es entero: {fecha1.Ext_EsFechaValida()} --> {(fecha1.Ext_EsFechaValida() ? fecha1.Ext_ToFecha().ToString():"")}");
+            Console.WriteLine($"{fecha2}: es entero: {fecha2.Ext_EsFechaValida()} --> {(fecha2.Ext_EsFechaValida() ? fecha1.Ext_ToFecha().ToString():"")}");
+            Console.WriteLine($"{fecha3}: es entero: {fecha3.Ext_EsFechaValida()} --> {(fecha3.Ext_EsFechaValida() ? fecha1.Ext_ToFecha().ToString():"")}");
+            Console.WriteLine($"{fecha4}: es entero: {fecha4.Ext_EsFechaValida()} --> {(fecha4.Ext_EsFechaValida() ? fecha1.Ext_ToFecha().ToString():"")}");
+            
         }
     }
 }
